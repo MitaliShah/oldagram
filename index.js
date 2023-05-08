44e6782c2fe178d7ed3c16a46d7c878b43381b91
Mitali Shah <mitalishaleenshah@gmail.com>
2023-05-08 10:04:05 -0500

    alt attribute for images

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ function loadCards (posts) {
     let list = posts.map(({ name, username, location, avatar, post, comment, likes }) => `
         <section>
             <div class="avatar-container">
-                <img class="avatar-img" src="${avatar}">
+                <img class="avatar-img" src="${avatar}" alt="Avatar Image">
                 <div class="avatar-info">
                     <p class="avatar-name">${name}</p>
                     <p class="avatar-location">${location}</p>
                 </div>
             </div>
             <div>
-                <img class="post-img" src="${post}">
+                <img class="post-img" src="${post}" alt="post-image">
             </div>
             <div class="bottom-info">
                 <div class="icons-container">                    
-                    <img id="likeIt" class="icon" src="./images/icon-heart.png">
-                    <img class="icon" src="./images/icon-dm.png">
-                    <img class="icon" src="./images/icon-comment.png">
+                    <img id="likeIt" class="icon" src="./images/icon-heart.png" alt="Heart Icon">
+                    <img class="icon" src="./images/icon-dm.png" alt="dm Icon">
+                    <img class="icon" src="./images/icon-comment.png" alt="comment Icon">
                 </div> 
                 <div class="likes-container">
                     <p>${likes} likes</p>
