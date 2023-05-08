ef4dce08d587f839862792aef5712124ad4275fe
Mitali Shah <mitalishaleenshah@gmail.com>
2023-05-08 08:56:17 -0500

    clicking on a hear icon increases the count and adds a red background to the icon and clicking it again decreases the count and removes the red background

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function loadCards (posts) {
                 <img class="post-img" src="${post}">
             </div>
             <div class="bottom-info">
-                <div class="icons-container">
-                    <img class="icon" src="./images/icon-heart.png">
+                <div class="icons-container">                    
+                    <img id="likeIt" class="icon" src="./images/icon-heart.png">
                     <img class="icon" src="./images/icon-dm.png">
                     <img class="icon" src="./images/icon-comment.png">
                 </div> 
@@ -30,11 +30,48 @@ function loadCards (posts) {
                 </div>
             </div>    
         </section>
-    `)    
+    `);
+    
     container.innerHTML = list;
 }
 
+function updateLikeText (post,index) {
+    let likesContainer = document.querySelectorAll(".likes-container p");
+    likesContainer[index].textContent = `${post.likes} likes`;
+}
+
+function handleLikes(posts) {
+    // Get the input for ALL the like icon
+    let likeIcons = document.querySelectorAll('#likeIt');
+    
+    // Use forEach to loop throught and get the specific icon element
+    likeIcons.forEach((icon, index) => {
+
+    // Individual post from posts using the index from forEach
+    let post = posts[index];
+    
+    // Eventlistner for each icon
+    icon.addEventListener("click", function() {
+            if(post.isLiked === true && post.likes >= 0){
+                post.isLiked = false;
+                post.likes -=1;
+                icon.classList.remove("isLiked");
+            } else if(post.isLiked === false && post.isLiked >= 0){
+                post.isLiked = true;
+                post.likes += 1;
+                icon.classList.add("isLiked");            
+            }
+            // Update the text in the card with the changed values for likes
+            updateLikeText(post,index);
+        });        
+   });   
+}
+
 loadCards(posts);
+handleLikes(posts);
+
+
+
 
 
 
